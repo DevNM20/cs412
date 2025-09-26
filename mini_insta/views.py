19f83467096d97ca48dcd6ede4cb6487b78fa46f
bmoges18 <bmoges18@bu.edu>
2025-09-26 15:42:30 -0400

    Added all files for deployment.

--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Profile
 # Create your views here.
 class ProfileListView(ListView):
@@ -8,3 +8,10 @@ class ProfileListView(ListView):
     model = Profile
     template_name = "mini_insta/show_all_profiles.html"
     context_object_name = "profiles"
+
+class ProfileDetailView(DetailView):
+    '''Display a single Profile.'''
+
+    model = Profile
+    template_name = "mini_insta/profile.html"
+    context_object_name = "profile"
