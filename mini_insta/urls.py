1ed06066bae7cb22096420c53599b77243aa26c5
bmoges18 <bmoges18@bu.edu>
2025-10-10 17:13:18 -0400

    Fixing stuff

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -14,5 +14,4 @@ urlpatterns = [
     path('post/<int:pk>', PostDetailView.as_view(), name="show_post"),
     path('post/<int:pk>/update', UpdatePostView.as_view(), name="update_post"),
     path('post/<int:pk>/delete', DeletePostView.as_view(), name="delete_post"),
-    
 ]
